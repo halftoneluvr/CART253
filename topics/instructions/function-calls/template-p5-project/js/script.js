b813d3a0870e4670b55164c334f8a37d218a7d7e
halftoneluvr <dobs27@hotmail.com>
2026-09-16 10:04:27 -0400

    Added comments to my now pink sink, or white sink and pink pink I suppose

--- topics/instructions/function-calls/template-p5-project/js/script.js
+++ topics/instructions/function-calls/template-p5-project/js/script.js
@@ -1,19 +1,31 @@
 /**
- * Function Calls 
+ * The Pink Page
  * Owen Dobson
  * 
- * Working on the function calls example.
+ * Sitting about in a power pink bathroom, the viewer is faced with the blankless of the sink (?). 
+ * They sit and stare its pinkness, and wonder why the AI in VS Code keeps trying to end my sentences for me!!!
+ * I have a mind, and it is pink too... the code written to "complete" me has a pseudo-mind, but it is not pink. 
+ * I prefer my inefficient pink mind. 
+ * 
+ * The program is non-interactive so as to force the viewer to do some real work. ARg! Stop it VS Code!!! 
  */
 
 "use strict";
 
+/**
+ * Creates canvas for the piece. 
+ */
 function setup() {
-    // Once at the begining of every setup.
+    // Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
+/**
+ * Draws my beautiful sink in the sea of pink. 
+ */
 function draw() {
-    // Every frame
+    // the pink 
     background(255, 240, 255);
+    // the sink
     ellipse(200, 280, 240, 320);
 }
