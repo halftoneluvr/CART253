a436d98a3e880e86a05f34a4d167d98ae42c56f5
halftoneluvr <dobs27@hotmail.com>
2026-09-16 18:20:29 -0400

    Started the Instructions Challenge

---
+++ topics/instructions-challenge/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Instructions Challenge
+ * Owen Dobson
+ * 
+ * Objective: Draw a Landscape 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
