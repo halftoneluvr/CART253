2b1a4d12a1ed66d94cb73193bf42412f8f629967
halftoneluvr <dobs27@hotmail.com>
2026-09-16 20:22:57 -0400

    Completed first two stages of flower

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -11,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-    createCanvas(1080, 566);
+    createCanvas(1080, 200);
 }
 
 
@@ -21,54 +21,33 @@ function setup() {
 function draw() {
     background(177, 245, 252);
     
-    // Grey elements
-    arrayTriangles();
-    
-    // People
-    createPeople();
-}
+    // Base of neon Grass
+    fill(194, 255, 5);
+    noStroke();
+    rect(0, 180, 1080, 566);
 
-function arrayTriangles(){
-     fill("grey");
-    noStroke();
-    rect(0, 540, 1080, 566);
-    rect(0, 0, 1080, 26);
-    triangle(1080, 566, 1080, 0, 650, 566);
-    fill(212, 212, 212);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 700, 566);
-    fill(192);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 750, 566);
-    fill(170);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 800, 566);
-    fill(222);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 850, 566);
-    fill(160);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 900, 566);
-    fill(288);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 950, 566);
-    fill(108);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 1000, 566);
-    fill(255);
-    noStroke();
-    triangle(1080, 566, 1080, 0, 1050, 566);
-}
+    // First Breakout
+    fill(179, 200, 99);
+    triangle(180, 180, 173, 175, 178, 170);
+    fill(156, 196, 90);
+    triangle(180, 180, 183, 178, 184, 170);
 
-function createPeople(){
-    fill(79, 44, 2);
-    ellipse(10, 520, 10);
-    triangle(10, 520, 5, 540, 15, 540);
-    fill(117, 68, 4);
-    ellipse(100, 510, 20, 23);
-    triangle(100, 510, 95, 540, 105, 540);
-    fill(163, 98, 7);
-    ellipse(200, 500, 28, 25);
-    triangle(200, 500, 190, 540, 210, 540);
-    
+    // Second Leaf 
+    fill(181, 205, 96);
+    triangle(360, 180, 357, 160, 350, 166);
+    fill(157, 186, 92);
+    triangle(360, 180, 366, 170, 370, 171);
+
+    // Third More Leaf
+
+
+    // Fourth Colour on Bud
+
+
+    // Fifth Basically a Flower 
+
+
+    // Sixth Bloom 
+
+
 }
