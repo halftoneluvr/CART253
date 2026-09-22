6fa46a86445f86767a58a04b94f27d2f9a7442e2
halftoneluvr <dobs27@hotmail.com>
2026-09-22 09:25:22 -0400

    Set up colour gradient from blue to green

--- Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg2)/js/script.js
+++ Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg2)/js/script.js
@@ -9,11 +9,24 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates canvas for drawing
 */
 function setup() {
+  createCanvas(223, 223);
+
+  // Use HSB color with values in the range 0-100.
+  colorMode(HSB, 100);
+
+  for (let x = 0; x < 223; x += 1) {
+    for (let y = 0; y < 223; y += 1) {
+      let hueValue = map(x, 0, 223, 67, 33);
+      stroke(hueValue, 100, 100);
+      point(x, y);
+    }
+  }
 
 }
+
 
 
 /**
