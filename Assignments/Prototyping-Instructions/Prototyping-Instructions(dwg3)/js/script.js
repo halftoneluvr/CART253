fc1b5748943317475f575c4bd21c2501f3ba4de0
halftoneluvr <dobs27@hotmail.com>
2026-09-23 15:04:44 -0400

    tried using pre-written inversion gradient code

--- Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg3)/js/script.js
+++ Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg3)/js/script.js
@@ -21,7 +21,8 @@ function setup() {
 */
 function draw() {
     //background set-up
-    background(255);
+    background(213, 255, 0);
+
 
     //Citron yellow elements 
     fill(173, 165, 19);
@@ -37,5 +38,17 @@ function draw() {
     fill(191, 213, 222);
     noStroke();
     ellipse(200,25, 85);
+    
+    loadPixels();
+     for (let y = 0; y < height; y++) {
+        let amount = map(y, 0, height, 0, 1);
+        for (let x = 0; x < width; x++) {
+        let index = (x + y * width) * 4;
+        pixels[index]     = lerp(pixels[index], 255 - pixels[index], amount);
+        pixels[index + 1] = lerp(pixels[index + 1], 255 - pixels[index + 1], amount);
+        pixels[index + 2] = lerp(pixels[index + 2], 255 - pixels[index + 2], amount);
+        }
+  }
+  updatePixels();
 
 }
