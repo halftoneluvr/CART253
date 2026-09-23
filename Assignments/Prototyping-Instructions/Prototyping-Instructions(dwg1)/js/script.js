7c3dbbe1109c29b95303712501b03184c1dd69d2
halftoneluvr <dobs27@hotmail.com>
2026-09-23 07:51:43 -0400

    Created the plate for branzino... been craving it so I'll draw it first!

--- Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg1)/js/script.js
+++ Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg1)/js/script.js
@@ -20,5 +20,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(150);
+    background(191, 213, 222);
+
+    drawPlate();
+}
+
+function drawPlate(){
+    fill(150);
+    noStroke();
+    ellipse(15, 113, 20);
+    ellipse(210, 113, 20);
+    ellipse(113, 175, 20);
+    ellipse(113, 50, 20);
+    
+    ellipse(123, 175, 12);
+    ellipse(103, 50, 12);
+    ellipse(123, 50, 12);
+    ellipse(103, 175, 12);
+    
+    ellipse(113, 113, 200, 120);
+    fill(170);
+    ellipse(113, 113, 190, 110);
 }
