90c201c0213ced31e5092917f3333d8e0be0fd3c
halftoneluvr <dobs27@hotmail.com>
2026-09-25 09:54:35 -0400

    Worked on the background... had some more fun w/ the alpha values

--- Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg3)/js/script.js
+++ Assignments/Prototyping-Instructions/Prototyping-Instructions(dwg3)/js/script.js
@@ -23,9 +23,36 @@ function draw() {
     //background set-up
     background(180, 205, 0);
     
+    //black ellipse
     fill('black');
     ellipse(225, height/2, 25, 300);
+
+    //Transparent black elements - various alpha values
+    fill(0, 0, 0, 150);
+    noStroke();
+    rect(30, 0, 7, 225);
+    rect(13, 0, 2, 225);
+    rect(130, 0, 14, 225);
+    rect(198, 0, 2, 225);
+    rect(203, 0, 2, 225);
+     rect(85, 0, 7, 225);
+
+    fill(0, 0, 0, 100);
+    rect(30, 0, 7, 225);
+    rect(40, 0, 21, 225);
+    rect(100, 0, 21, 225);
+    rect(186, 0, 2, 225);
+    rect(122, 0, 2, 225);
+
+    fill(0, 0, 0, 120);
+    rect(40, 0, 2, 225);
+    rect(146, 0, 2, 225);
+    rect(180, 0, 7, 225);
+    rect(2, 0, 7, 225);
+     rect(65, 0, 2, 225);
+
     
+    //white ellipses
     fill('white');
     strokeWeight(7);
     stroke('black');
@@ -33,6 +60,11 @@ function draw() {
     strokeWeight(6);
     ellipse(40, 2*height/3, 300, 50);
     
-    
+    //Chartreuse circles**
+    fill(180, 205, 0);
+    noStroke();
+    ellipse(15, 2*height/3, 25);
+    ellipse(35, 2*height/3, 20);
+    ellipse(50, 2*height/3, 15);
     
 }
